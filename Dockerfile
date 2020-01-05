770da172784ef5d576c6795e66a7879730ecf46f
Cyrus Roshan <cyrusroshan@users.noreply.github.com>
2020-01-05 14:13:21 -0800

    Add multi-architecture containers.

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM golang:1.12
-WORKDIR /app
-COPY ./ ./
-RUN GOOS=linux GOARCH=arm go build -o demo-go
-
-FROM arm32v7/alpine
-COPY --from=0 /app/demo-go /bin/demo-go
-ENTRYPOINT ["/bin/demo-go"]
-EXPOSE 8080
---
+++ scripts/build-containers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+docker build -t deviceplane/demo-go:amd64-${VERSION} -f dockerfiles/amd64/Dockerfile --build-arg version=${VERSION} .
+docker build -t deviceplane/demo-go:arm32v6-${VERSION} -f dockerfiles/arm32v6/Dockerfile --build-arg version=${VERSION} .
+docker build -t deviceplane/demo-go:arm64v8-${VERSION} -f dockerfiles/arm64v8/Dockerfile --build-arg version=${VERSION} .
+
+docker push deviceplane/demo-go:amd64-${VERSION}
+docker push deviceplane/demo-go:arm32v6-${VERSION}
+docker push deviceplane/demo-go:arm64v8-${VERSION}
+
+docker manifest create deviceplane/demo-go:${VERSION} deviceplane/demo-go:amd64-${VERSION} deviceplane/demo-go:arm32v6-${VERSION} deviceplane/demo-go:arm64v8-${VERSION}
+docker manifest annotate deviceplane/demo-go:${VERSION} deviceplane/demo-go:arm32v6-${VERSION} --os linux --arch arm
+docker manifest annotate deviceplane/demo-go:${VERSION} deviceplane/demo-go:arm64v8-${VERSION} --os linux --arch arm64
